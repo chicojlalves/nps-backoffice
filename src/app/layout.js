4d5e8930938475c25e35ee7475f32794178cf4f9
Chicojlalves <chicojlalves@gmail.com>
2026-06-10 11:43:50 -0300

    feat: adiciona Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +31,10 @@ export default function RootLayout({ children }) {
         gtag('js', new Date());
         gtag('config', 'G-011XED6QZD');
       `}</Script>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
